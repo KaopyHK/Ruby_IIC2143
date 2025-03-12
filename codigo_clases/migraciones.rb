445e719b94395e050bec67878dbbe6aa25d138ca
Kaopy <benjamingomez705@gmail.com>
2025-03-12 11:58:02 -0300

    codigo

---
+++ codigo_clases/migraciones.rb
@@ -0,0 +1,3 @@
+# Las migraciones son instrucciones que permiten evolucionar el esquema de base de datos con el tiempo, de manera reproducible
+# Cada migracion se puede pensar como una version del esquema en la base de datos
+# Se encuentran en la carpeta db
